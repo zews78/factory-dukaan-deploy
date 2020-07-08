730a4fdbc74f2575ef211f79851f80a71ef7ecb6
zews78 <shariquealam52@gmail.com>
2020-07-08 21:42:27 +0530

    added login page

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+app.use(express.static('public'));
+// to recognise incoming request object as array/string
+app.use(express.urlencoded({extended: false}));
+
+app.set('views', [path.join(__dirname, 'views'),
+                  path.join(__dirname, 'views/auth/')]);
+
+
+
+app.get('/login',(req, res)=>{
+    res.render('login.ejs');
+})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server started @${PORT}`));
