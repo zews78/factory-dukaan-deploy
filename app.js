c4e2487041545f67513ff5252a671df46e7d0e06
Anirudh <anirudhganwal06@gmail.com>
2020-07-09 12:14:25 +0530

    added some comments in app.js

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Setting public path
 app.use(express.static('public'));
+
 // to recognise incoming request object as array/string
 app.use(express.urlencoded({extended: false}));
+
+// setting views folder as the default view folder
 app.set('views', path.join(__dirname, 'views'));
 
+// importing all necessary routes
 const authRoutes = require('./routes/auth');
 
+// using all necessary routes
 app.use(authRoutes);
 
 const PORT = 3001;
