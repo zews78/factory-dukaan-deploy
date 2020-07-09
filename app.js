a6d6999e2efafb9362a67ef6ba2c6334d48d94f1
Anirudh <anirudhganwal06@gmail.com>
2020-07-09 12:07:48 +0530

    refactored code

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static('public'));
 // to recognise incoming request object as array/string
 app.use(express.urlencoded({extended: false}));
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('views', [path.join(__dirname, 'views'),
-                  path.join(__dirname, 'views/auth/')]);
+const authRoutes = require('./routes/auth');
 
+app.use(authRoutes);
 
-
-app.get('/login',(req, res)=>{
-    res.render('login.ejs');
-})
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server started @${PORT}`));
+const PORT = 3001;
+app.listen(PORT, () =>
+	console.log(`Server started @${PORT}`)
+);
