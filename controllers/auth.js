b9bf6fec8d190eb2e225b34eee43f814f26f542a
Anirudh <anirudhganwal06@gmail.com>
2020-07-10 13:25:58 +0530

    user saved on login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,21 @@
-// const firebase = require('../firebase');
+const firebase = require('../firebase');
 
 exports.getLogin = (req, res) => {
 	res.render('auth/login.ejs');
 };
 
-exports.postLogin = (req, res) => {
+exports.postLogin = async(req, res) => {
 	console.log(req.body);
+	if (req.body.additionalUserInfo.isNewUser) {
+		// Create a new user
+		const userData = {};
+		const uid = req.body.user.uid;
+		userData.mobile = req.body.user.phoneNumber;
+		firebase.firestore().collection('users').doc(uid).set(userData);
+	} else {
+		// Login
+		console.log('for login');
+	}
 	res.json({message: 'Success'});
 };
 
