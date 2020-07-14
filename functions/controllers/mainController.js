38dd383c6e1eacbb6f5705a7fd1e038895167b47
Anirudh <anirudhganwal06@gmail.com>
2020-07-14 11:50:47 +0530

    added a todo in get products

--- functions/controllers/mainController.js
+++ functions/controllers/mainController.js
@@ -1,49 +1,55 @@
-const firebase = require("../firebase");
+const firebase = require('../firebase');
 
-const isAuth = require("../utils/isAuth");
+const isAuth = require('../utils/isAuth');
 
-exports.getHome = async (req, res) => {
-  const auth = (await isAuth(req))[0];
-  res.render("../views/main/home.hbs", { auth });
+exports.getHome = async(req, res) => {
+	const auth = (await isAuth(req))[0];
+	res.render('../views/main/home.hbs', {auth});
 };
 
-exports.getProducts = async (req, res) => {
-  const auth = (await isAuth(req))[0];
-  const products = await firebase
-    .firestore()
-    .collection("products")
-    .orderBy("price");
-  let queriedProduct = null;
+exports.getProducts = async(req, res) => {
+	// Search - search in keywords
+	// Filter - Price(asc, desc), new
+	const auth = (await isAuth(req))[0];
+	const products = await firebase.firestore()
+		.collection('products')
+		.orderBy('price');
+	let queriedProduct = null;
 
-  if (req.query["tags"]) {
-    queriedProduct = await products
-      .where("tags", "array-contains-any", req.query["tags"].split(" "))
-      .get();
-  } else {
-    queriedProduct = await products.get();
-  }
+	if (req.query['tags']) {
+		queriedProduct = await products
+			.where('tags', 'array-contains-any', req.query['tags'].split(' '))
+			.get();
+	} else {
+		queriedProduct = await products.get();
+	}
 
-  res.render("../views/main/products.hbs", { auth });
+	res.render('../views/main/products.hbs', {
+		auth,
+		products: queriedProduct
+	});
 };
 
-exports.postProduct = async (req, res) => {
-  try {
-    await firebase.firestore().collection("products").add(req.body);
-    res.redirect("/user/products");
-  } catch (err) {
-    console.log(err);
-  }
+exports.postProduct = async(req, res) => {
+	try {
+		await firebase.firestore()
+			.collection('products')
+			.add(req.body);
+		res.redirect('/user/products');
+	} catch (err) {
+		console.log(err);
+	}
 };
 
-exports.patchProduct = async (req, res) => {
-  try {
-    await firebase
-      .firestore()
-      .collection("products")
-      .doc(req.params.productId)
-      .update(req.body);
-    res.redirect("/user/products");
-  } catch (err) {
-    console.log(err);
-  }
+exports.patchProduct = async(req, res) => {
+	try {
+		await firebase
+			.firestore()
+			.collection('products')
+			.doc(req.params.productId)
+			.update(req.body);
+		res.redirect('/user/products');
+	} catch (err) {
+		console.log(err);
+	}
 };
