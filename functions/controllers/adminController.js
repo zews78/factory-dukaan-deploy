cb2f3e6178ce3d16be9280bc77a9eba618285632
Anirudh <anirudhganwal06@gmail.com>
2020-07-16 21:57:26 +0530

    created no product found ui and fixed some query related bug

--- functions/controllers/adminController.js
+++ functions/controllers/adminController.js
@@ -3,12 +3,12 @@ const keywordGenerator = require('../utils/keywordGenerator');
 
 exports.getDashboard = async(req, res) => {
 	res.redirect('/admin/products');
-	// res.render('../views/admin/dashboard.hbs');
+	// res.render('admin/dashboard');
 };
 
 exports.getProducts = async(req, res) => {
 	try {
-		const limit = +req.query.limit || 10;
+		const limit = +req.query.limit || 15;
 
 		// Creating a reference to products
 		let productsRef = firebase.firestore()
@@ -22,10 +22,11 @@ exports.getProducts = async(req, res) => {
 		// Sorting
 		if (req.query.sortBy) {
 			productsRef = productsRef.orderBy(req.query.sortBy, req.query.order || 'asc');
-		}
-		if (req.query.sortBy !== 'createdOn') {
+		} else {
 			productsRef = productsRef.orderBy('createdOn', 'desc');
 		}
+
+		const paginationValidationRef = productsRef;
 
 		// If there is a query param after/before
 		if (req.query.after) {
@@ -49,6 +50,7 @@ exports.getProducts = async(req, res) => {
 		// Fetching the productsSnapshot
 		const productsSnapshot = await productsRef.get();
 
+
 		// Making products ready for passing to templating engine for rendering
 		const products = [];
 		productsSnapshot.forEach((product) => {
@@ -60,34 +62,46 @@ exports.getProducts = async(req, res) => {
 			});
 		});
 
+		if (products.length === 0) {
+			res.render('admin/products', {
+				products: [],
+				queryParams: req.query
+			});
+			return;
+		}
+
 		// Pagination buttons after/before links
 		let last = productsSnapshot.docs[productsSnapshot.docs.length - 1];
 		let first = productsSnapshot.docs[0];
 
-		const prevSnapshot = await firebase
-			.firestore()
-			.collection('products')
-			.orderBy('price', 'desc')
-			.endBefore(first)
+		const queryParams = {...req.query};
+
+		// Checking that there is a prev page available
+		const prevSnapshot = await paginationValidationRef.endBefore(first)
 			.limitToLast(1)
 			.get();
-		if (prevSnapshot.docs.length > 0) {
-			first = '?before=' + first.id;
-		} else {
+		if (prevSnapshot.docs.length === 0) {
 			first = false;
+		} else {
+			first = first.id;
 		}
 
-		const nextSnapshot = await firebase
-			.firestore()
-			.collection('products')
-			.orderBy('price', 'desc')
-			.startAfter(last)
+		// Checking that there is a next page available
+		const nextSnapshot = await paginationValidationRef.startAfter(last)
 			.limit(1)
 			.get();
-		if (nextSnapshot.docs.length > 0) {
-			last = '?after=' + last.id;
-		} else {
+		if (nextSnapshot.docs.length === 0) {
 			last = false;
+		} else {
+			last = last.id;
+		}
+
+		// Generating query string
+		let queryString = '?';
+		for (let query in queryParams) {
+			if (!(query === 'before' || query === 'after')) {
+				queryString += query + '=' + queryParams[query] + '&';
+			}
 		}
 
 		res.render('admin/products', {
@@ -96,7 +110,8 @@ exports.getProducts = async(req, res) => {
 				prev: first,
 				next: last
 			},
-			queryParams: req.query
+			queryParams: req.query,
+			queryString
 		});
 	} catch (err) {
 		console.log(err);
