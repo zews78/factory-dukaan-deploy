2512be69c18744ffbf0ec974ef5ccef352bfd542
Anirudh <anirudhganwal06@gmail.com>
2020-07-16 13:58:36 +0530

    implemented pagination in products page

--- functions/controllers/adminController.js
+++ functions/controllers/adminController.js
@@ -7,11 +7,12 @@ exports.getDashboard = async(req, res) => {
 
 exports.getProducts = async(req, res) => {
 	try {
+		const limit = 1;
 
 		// Creating a reference to products
 		let productsRef = firebase.firestore()
 			.collection('products')
-			.orderBy('createdOn', 'desc');
+			.orderBy('price', 'desc');
 
 		// If there is a query param after/before
 		if (req.query.after) {
@@ -19,24 +20,25 @@ exports.getProducts = async(req, res) => {
 				.collection('products')
 				.doc(req.query.after)
 				.get();
-			productsRef = productsRef.startAfter(lastSnapshot);
+			productsRef = productsRef.startAfter(lastSnapshot)
+				.limit(limit);
 		} else if (req.query.before) {
 			let firstSnapshot = await firebase.firestore()
 				.collection('products')
 				.doc(req.query.before)
 				.get();
-			productsRef = productsRef.endBefore(firstSnapshot);
+			productsRef = productsRef.endBefore(firstSnapshot)
+				.limitToLast(limit);
+		} else {
+			productsRef = productsRef.limit(limit);
 		}
-
-		// Settings limit
-		productsRef = productsRef.limit(15);
 
 		// Fetching the productsSnapshot
 		const productsSnapshot = await productsRef.get();
 
 		// Making products ready for passing to templating engine for rendering
 		const products = [];
-		productsSnapshot.forEach(product => {
+		productsSnapshot.forEach((product) => {
 			products.push({
 				id: product.id,
 				...product.data(),
@@ -46,17 +48,43 @@ exports.getProducts = async(req, res) => {
 		});
 
 		// Pagination buttons after/before links
-		const after = productsSnapshot.docs[productsSnapshot.docs.length - 1].id;
-		const before = productsSnapshot.docs[0].id;
+		let last = productsSnapshot.docs[productsSnapshot.docs.length - 1];
+		let first = productsSnapshot.docs[0];
+
+		const prevSnapshot = await firebase
+			.firestore()
+			.collection('products')
+			.orderBy('price', 'desc')
+			.endBefore(first)
+			.limitToLast(1)
+			.get();
+		if (prevSnapshot.docs.length > 0) {
+			first = '?before=' + first.id;
+		} else {
+			first = false;
+		}
+
+		const nextSnapshot = await firebase
+			.firestore()
+			.collection('products')
+			.orderBy('price', 'desc')
+			.startAfter(last)
+			.limit(1)
+			.get();
+		if (nextSnapshot.docs.length > 0) {
+			last = '?after=' + last.id;
+		} else {
+			last = false;
+		}
 
 		res.render('../views/admin/products.hbs', {
 			products,
 			links: {
-				previous: `?before=${before}`,
-				next: `?after=${after}`
+				prev: first,
+				next: last
 			}
 		});
-	} catch(err) {
+	} catch (err) {
 		console.log(err);
 	}
 };
